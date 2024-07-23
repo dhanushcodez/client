d18cb0d01485f505031704ae2fb3947f26368c72
dhanushcodez <112488177+dhanushcodez@users.noreply.github.com>
2024-07-23 19:42:33 +0530

    Update Policy.js

--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -13,13 +13,13 @@ const Policy = () => {
           />
         </div>
         <div className="col-md-4">
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
+          <p>We collect personal information like name, email, shipping address, and payment details during the purchase process.</p>
+          <p>Your information is used to process orders, improve our services, and send promotional offers.</p>
+          <p>We implement strict security measures to protect your data from unauthorized access and breaches</p>
+          <p> We dont sell or share your personal information to third parties, except to essential service providers</p>
+          <p>We use cookies to enhance your browsing experience and analyze website traffic.</p>
+          <p>You have the right to access, update your personal information at any time.</p>
+          <p>For any privacy-related inquiries, please contact our support team.</p>
         </div>
       </div>
     </Layout>
