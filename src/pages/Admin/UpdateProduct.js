85436f0f2aaa66649db1dcc9cc937ac4ca28ac27
dhanushcodez <112488177+dhanushcodez@users.noreply.github.com>
2024-07-23 18:48:04 +0530

    Update UpdateProduct.js

--- src/pages/Admin/UpdateProduct.js
+++ src/pages/Admin/UpdateProduct.js
@@ -24,7 +24,7 @@ const UpdateProduct = () => {
   const getSingleProduct = async () => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/get-product/${params.slug}`
+        `https://urban-it-server.onrender.com/api/v1/product/get-product/${params.slug}`
       );
       setName(data.product.name);
       setId(data.product._id);
@@ -45,7 +45,7 @@ const UpdateProduct = () => {
   //get all category
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("/api/v1/category/get-category");
+      const { data } = await axios.get("https://urban-it-server.onrender.com/api/v1/category/get-category");
       if (data?.success) {
         setCategories(data?.category);
       }
