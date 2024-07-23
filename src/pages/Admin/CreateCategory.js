8de72a06cdd3e9b8f796afa5c0db2221f5fa9792
dhanushcodez <112488177+dhanushcodez@users.noreply.github.com>
2024-07-23 19:28:54 +0530

    Update CreateCategory.js

--- src/pages/Admin/CreateCategory.js
+++ src/pages/Admin/CreateCategory.js
@@ -52,7 +52,7 @@ const CreateCategory = () => {
     e.preventDefault();
     try {
       const { data } = await axios.put(
-        `/api/v1/category/update-category/${selected._id}`,
+        `https://urban-it-server.onrender.com/api/v1/category/update-category/${selected._id}`,
         { name: updatedName }
       );
       if (data.success) {
@@ -72,7 +72,7 @@ const CreateCategory = () => {
   const handleDelete = async (pId) => {
     try {
       const { data } = await axios.delete(
-        `/api/v1/category/delete-category/${pId}`
+        `https://urban-it-server.onrender.com/api/v1/category/delete-category/${pId}`
       );
       if (data.success) {
         toast.success(`category is deleted`);
