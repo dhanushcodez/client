831fadf244b27da1e017b385d528bf3125ff25d1
dhanushcodez <112488177+dhanushcodez@users.noreply.github.com>
2024-07-23 12:49:43 +0530

    Update Prices.js

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,32 +1,32 @@
 export const Prices = [
   {
     _id: 0,
-    name: "Rs-0 to 19",
-    array: [0, 19],
+    name: "Below Rs-1000",
+    array: [0, 1000],
   },
   {
     _id: 1,
-    name: "Rs-20 to 39",
-    array: [20, 39],
+    name: "Rs-1000 to 2000",
+    array: [1000, 2000],
   },
   {
     _id: 2,
-    name: "Rs-40 to 59",
-    array: [40, 59],
+    name: "Rs-2000 to 3000",
+    array: [2000, 3000],
   },
   {
     _id: 3,
-    name: "Rs-60 to 79",
-    array: [60, 79],
+    name: "Rs-3000 to 5000",
+    array: [3000, 5000],
   },
   {
     _id: 4,
-    name: "Rs-80 to 99",
-    array: [80, 99],
+    name: "Rs-5000 to 10000",
+    array: [5000, 10000],
   },
   {
     _id: 4,
-    name: "Rs-100 or more",
-    array: [100, 9999],
+    name: "Rs-10000 or more",
+    array: [10000, 99999],
   },
 ];
